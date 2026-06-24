092b2f87bae3c9d2b510f705a132f9d418675d98
Ferdian-R <ferdianrahman209@gmail.com>
2026-06-24 10:30:25 +0700

    fix: ensure old conflicting users table is dropped before recreating

--- scripts/schema_survey.sql
+++ scripts/schema_survey.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS ole_has_permissions;
+DROP TABLE IF EXISTS model_has_roles;
+DROP TABLE IF EXISTS permissions;
+DROP TABLE IF EXISTS oles;
+DROP TABLE IF EXISTS users;
 SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `survey_answer_options`;
@@ -172,22 +177,28 @@ ame varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   UNIQUE KEY users_email_unique (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS oles (
+CREATE TABLE IF NOT EXISTS 
+oles (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   
 ame varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY oles_name_unique (
+  UNIQUE KEY 
+oles_name_unique (
 ame)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS model_has_roles (
-  ole_id bigint(20) unsigned NOT NULL,
+  
+ole_id bigint(20) unsigned NOT NULL,
   model_type varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   model_id bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (ole_id,model_id,model_type),
+  PRIMARY KEY (
+ole_id,model_id,model_type),
   KEY model_has_roles_model_id_model_type_index (model_id,model_type),
-  CONSTRAINT model_has_roles_role_id_foreign FOREIGN KEY (ole_id) REFERENCES oles (id) ON DELETE CASCADE
+  CONSTRAINT model_has_roles_role_id_foreign FOREIGN KEY (
+ole_id) REFERENCES 
+oles (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS permissions (
@@ -197,10 +208,17 @@ ame varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS ole_has_permissions (
+CREATE TABLE IF NOT EXISTS 
+ole_has_permissions (
   permission_id bigint(20) unsigned NOT NULL,
-  ole_id bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (permission_id,ole_id),
-  CONSTRAINT ole_has_permissions_permission_id_foreign FOREIGN KEY (permission_id) REFERENCES permissions (id) ON DELETE CASCADE,
-  CONSTRAINT ole_has_permissions_role_id_foreign FOREIGN KEY (ole_id) REFERENCES oles (id) ON DELETE CASCADE
+  
+ole_id bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (permission_id,
+ole_id),
+  CONSTRAINT 
+ole_has_permissions_permission_id_foreign FOREIGN KEY (permission_id) REFERENCES permissions (id) ON DELETE CASCADE,
+  CONSTRAINT 
+ole_has_permissions_role_id_foreign FOREIGN KEY (
+ole_id) REFERENCES 
+oles (id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
